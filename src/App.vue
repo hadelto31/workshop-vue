35dee087a7630a944b45ae897cddcac2a023bb1e
harolete10 <hadelto@hotmail.com>
2023-06-30 14:43:09 -0500

    1commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <work-cards-vue  vacante="SENIOR BACK END DEVELOPER - KNOWLEDGE GRAPH" habilidades="Graphql"  empresa="20 moves" ubicacion="Remoto" sueldo="2M -3M COP" vacantes="3 vacantes" dias="hace 3 dias"/> 
+    <work-cards-vue   vacante="Director comercial " habilidades="Ingles" empresa="Bankuish" ubicacion="Remoto" sueldo="3K - 4K US" vacantes="1 vacante" dias="hace 1 dia"/>
+    <work-cards-vue vacante="Gerente nacional de ventas " habilidades="Business Closure" empresa="Confidencial" ubicacion="Bogota" sueldo="6M - 13M COP" vacantes="1 vacante" dias="hace 2 dias"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WorkCardsVue from './components/WorkCards.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WorkCardsVue
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/WorkCards.vue
@@ -0,0 +1,62 @@
+
+
+<template>
+  <b-card mt-5>
+    <b-container>
+      <b-row>
+        <b-col class="text-center" col lg="1">
+          <b-img
+            src= "https://peaku.io/img/logo-blue.png"
+            img-alt="Card image"
+            img-left
+            class="mb-3"
+            style="width: 60%; height: 60%; fluid "
+          />
+        </b-col>
+        <b-col>
+          <b-card-text>
+            <div>
+              
+                <h6 >{{ vacante }}</h6>
+                <p >{{ habilidades }}</p>
+              
+            </div>
+
+            <p>{{ empresa }}</p>
+            <p>{{ ubicacion }}</p>
+            <p>{{ sueldo }}</p>
+            <p>{{ vacantes }}</p>
+            <p>{{ dias }}</p>
+          </b-card-text>
+        </b-col>
+      </b-row>
+    </b-container>
+  </b-card>
+</template>
+
+
+
+
+<script>
+export default {
+  props: {
+    rutaimg: String,
+    vacante: String,
+    habilidades: String,
+    empresa: String,
+    ubicacion: String,
+    sueldo: String,
+    vacantes: Number,
+    dias: String,
+  },
+};
+</script>
+
+<style scoped>
+h6, p {
+  display: inline-block;
+  margin: 0;
+  margin-right: 20px;
+}
+</style>
+
